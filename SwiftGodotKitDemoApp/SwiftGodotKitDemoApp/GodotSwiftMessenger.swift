921960fc71bb9baa70483cc2a2b21ec5298c5824
Xander Carruth <acarruth2018@gmail.com>
2025-06-06 12:13:24 -0400

    Small comments

--- SwiftGodotKitDemoApp/SwiftGodotKitDemoApp/GodotSwiftMessenger.swift
+++ SwiftGodotKitDemoApp/SwiftGodotKitDemoApp/GodotSwiftMessenger.swift
@@ -10,8 +10,13 @@ import SwiftGodot
 @Godot
 class GodotSwiftMessenger: Object {
     public static let shared = GodotSwiftMessenger()
+    
+    // Signals color of current active panel to start ColorSheetView
     @Signal var panelSelected: SignalWithArguments<String>
+    // Signals when to return from CharacterGameView back to GameStartView
     @Signal var gameEnded: SimpleSignal
+    // Signals when to change rotating cube color
     @Signal var changeCubeColor: SignalWithArguments<Int>
+    // Signals when loading spinner should disappear, otherwise red background flashes
     @Signal var sceneReady: SignalWithArguments<String>
 }
